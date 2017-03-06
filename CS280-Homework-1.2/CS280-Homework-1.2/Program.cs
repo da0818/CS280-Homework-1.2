2fae95d6765c755096ef7cecd2e1b5a67aa43182
da0818 <chuntawu@gmail.com>
2017-03-06 15:48:38 +0800

    新增pokemon類別

--- CS280-Homework-1.2/CS280-Homework-1.2/Program.cs
+++ CS280-Homework-1.2/CS280-Homework-1.2/Program.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace CS280_Homework_1._2
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Pokemon pokemon = new Pokemon();
+            pokemon.Name = "Charmander";
+            pokemon.Number = 4f;
+            pokemon.Height = 2.0f;
+            pokemon.Weight = 18.7f;
+            pokemon.Category = "Lizard";
+            pokemon.Abilities = "Blaze";
+            pokemon.Gender = "M";
+            Console.WriteLine("名字:{0}, 編號{1}", pokemon.Name, pokemon.Number);
         }
     }
 }
